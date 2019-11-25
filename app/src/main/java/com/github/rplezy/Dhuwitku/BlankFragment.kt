60b61d4fd01a24c665268fdd11374807486c3541
ferdiyusufsaputra <ferdiyusufsaputra@gmail.com>
2019-11-26 00:35:14 +0700

    First

---
+++ app/src/main/java/com/github/rplezy/Dhuwitku/BlankFragment.kt
@@ -0,0 +1,27 @@
+package com.github.rplezy.Dhuwitku
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class BlankFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = BlankFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+}
+
---
+++ app/src/main/java/com/github/rplezy/Dhuwitku/TemplateFragment.kt
@@ -0,0 +1,28 @@
+package com.github.rplezy.Dhuwitku
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TemplateFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TemplateFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_template, container, false)
+    }
+
+
+}
