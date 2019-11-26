cad353a31a869ed2e4a1d6e0649961c514dab916
ferdiyusufsaputra <ferdiyusufsaputra@gmail.com>
2019-11-26 13:24:21 +0700

    Home

--- app/src/main/java/com/github/rplezy/Dhuwitku/Login.kt
+++ app/src/main/java/com/github/rplezy/Dhuwitku/Login.kt
@@ -1,12 +1,18 @@
 package com.github.rplezy.Dhuwitku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        ButtonLogin.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
