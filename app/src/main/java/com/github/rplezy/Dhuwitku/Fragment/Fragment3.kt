b64fc29a1db16b37fed01d079536ec36b1ca1509
ferdiyusufsaputra <ferdiyusufsaputra@gmail.com>
2019-12-10 07:11:41 +0700

    Fix generator QR

--- app/src/main/java/com/github/rplezy/Dhuwitku/Fragment/Fragment3.kt
+++ app/src/main/java/com/github/rplezy/Dhuwitku/Fragment/Fragment3.kt
@@ -1,13 +1,15 @@
 package com.github.rplezy.Dhuwitku.Fragment
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import com.github.rplezy.Dhuwitku.QrGenerate
 import com.github.rplezy.Dhuwitku.R
+import kotlinx.android.synthetic.main.fragment3.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,7 +22,14 @@ class Fragment3 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment3, container, false)
+
+        btn_kirim.setOnClickListener{
+            val intent = Intent(context, QrGenerate::class.java)
+            startActivity(intent)
+        }
     }
+
+
 
 
 }
---
+++ app/src/main/java/com/github/rplezy/Dhuwitku/QrGenerate.kt
@@ -0,0 +1,52 @@
+package com.github.rplezy.Dhuwitku
+
+import android.content.Context
+import android.graphics.Point
+import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.WriterException
+import kotlinx.android.synthetic.main.activity_qr_generate.*
+
+
+class QrGenerate : AppCompatActivity() {
+    private val TAG: String = QrGenerate::class.java.getName()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_generate)
+
+        start.setOnClickListener {
+            edt_value.text.toString().trim()
+            if (edt_value.length() > 0) {
+                val manager =
+                    getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                val display = manager.defaultDisplay
+                val point = Point()
+                display.getSize(point)
+                val width = point.x
+                val height = point.y
+                var smallerDimension = if (width < height) width else height
+                smallerDimension = smallerDimension * 3 / 4
+                val qrgEncoder = QRGEncoder(
+                    edt_value.text.toString(), null,
+                    QRGContents.Type.TEXT,
+                    smallerDimension
+                )
+                try {
+                    val bitmap = qrgEncoder.encodeAsBitmap()
+                    qr_image.setImageBitmap(bitmap)
+                } catch (e: WriterException) {
+                    Log.v(TAG, "error")
+                }
+            } else {
+                edt_value.error = "Required"
+            }
+
+        }
+
+    }
+}
