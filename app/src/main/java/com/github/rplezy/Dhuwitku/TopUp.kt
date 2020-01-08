62790704b8810ddc1e8abee6dcede971c49e9bcd
ferdiyusufsaputra <ferdiyusufsaputra@gmail.com>
2020-01-08 07:35:58 +0700

    chnage layout topup

---
+++ app/src/main/java/com/github/rplezy/Dhuwitku/TopUp.kt
@@ -0,0 +1,20 @@
+package com.github.rplezy.Dhuwitku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
+class TopUp : AppCompatActivity() {
+    lateinit var mAdView : AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_up)
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+    }
+}
