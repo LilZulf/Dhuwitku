df36afde0cb2fdb2fe602d1cb4683f968d0df599
ferdiyusufsaputra <ferdiyusufsaputra@gmail.com>
2020-02-04 08:04:44 +0700

    addTransaksi

---
+++ app/src/main/java/com/github/rplezy/Dhuwitku/Model/AddTransaksi.kt
@@ -0,0 +1,8 @@
+package com.github.rplezy.Dhuwitku.Model
+
+data class AddTransaksi(
+	val code: Int? = null,
+	val data: Any? = null,
+	val message: String? = null,
+	val status: Boolean? = null
+)
