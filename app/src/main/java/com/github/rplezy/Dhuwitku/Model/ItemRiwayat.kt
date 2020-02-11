e6b786ee54e977d81dc44c4717a59c53eac624dd
aldevxix <aldevxix@gmail.com>
2020-02-11 12:26:30 +0700

    (+) Prerelease 1

---
+++ app/src/main/java/com/github/rplezy/Dhuwitku/Model/ItemRiwayat.kt
@@ -0,0 +1,9 @@
+package com.github.rplezy.Dhuwitku.Model
+
+data class ItemRiwayat(
+	val pemasukan: String? = null,
+	val pengeluaran: String? = null,
+	val id_transaksi: String? = null,
+	val tanggal: String? = null,
+	val id_user: String? = null
+)
